92822ab5b8864b2ce5d08f1a93eb44f09715fb8c
Nguyen Van Tien Hai <nvthai061105@gmail.com>
2025-10-15 17:21:51 +0700

    fix

--- front-end/app/actions.ts
+++ front-end/app/actions.ts
@@ -162,7 +162,7 @@ export const signInWithOAuthAction = async (formData: FormData) => {
   const baseUrl = isDev
     ? "http://localhost:3000"
     : process.env.NEXT_PUBLIC_SITE_URL ||
-      "https://hola-rental-client.onrender.com";
+      "https://thue-deploy.onrender.com";
   const redirectTo = `${baseUrl}/auth/callback`;
 
   console.log("Sending redirectTo to Supabase:", redirectTo);
